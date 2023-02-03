3f5a5d2a219e78d6dc51d7da47f58da19c894ef4
Simen <simdevgen@outlook.com>
2023-02-03 14:07:40 +0100

    Addon

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-console.log('Hello');
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const app = express();
+const PORT = process.env.PORT || 3333;
+app.use(cors());app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('Hei på dere');
+});
+
+// Listening to server
+app.listen(PORT, () => {
+    console.log(`The application is now listening to ${PORT}`)
+})
