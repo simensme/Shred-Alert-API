7b8a7227c1963f760d0fbb33af9c68d5ebef94da
EirikRH <eirikrhansen@gmail.com>
2023-02-07 13:37:24 +0100

    Implementing login/token functionality

--- server.js
+++ server.js
@@ -1,17 +1,55 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-const { testQuery } = require('./services/database');
+const { getUserByUsername } = require('./services/database');
 const app = express();
 const PORT = process.env.PORT || 3333;
 
 app.use(cors());
 app.use(express.json());
 
+const APP_SECRET = 'søtt-griseri';
 
-app.get('/', async (req, res) => {
-    const test = await testQuery();
-    res.send(test);
+
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+
+  try{
+
+    const user = await getUserByUsername(username);
+
+    if(!user){
+      res.status(401).send({error: 'Unknown user - not found'});
+      return;
+    }
+
+    if(password !== user.password){
+      res.status(401).send({error: 'Wrong password!'});
+      return;
+    }
+
+    const token = jwt.sign({
+      id: user.id,
+      username: user.username
+    }, Buffer.from(APP_SECRET, 'base64'));
+
+    res.json({ token });
+
+  } catch(error){
+    res.status(500).send({error: error.message})
+  }
+});
+
+
+app.get('/session', async (req, res) => {
+  const token = req.headers['x-token'];
+  
+  try{
+    const palyoard = jwt.verify(token, Buffer.from(APP_SECRET, 'Base64'));
+    res.json({message: `You are logged in as ${payload.username}`});
+  }catch(error){
+    res.status(401).send({error: 'Invalid token'});
+  }
 });
 
 // Listening to server
--- services/database.js
+++ services/database.js
@@ -1,21 +1,27 @@
 
 const {Pool} = require('pg');
-const POSTGRES_URL = process.env.POSTGRES_URL || 'postgres://postgres:1234@localhost:5432/ShredTest';
+const POSTGRES_URL = process.env.POSTGRES_URL || 'postgres://postgres:1234@localhost:5432/shreddatabase';
 
 const database = new Pool({
     connectionString: POSTGRES_URL
 })
 
-async function testQuery(){
-    const result = await database.query(`
+
+async function getUserByUsername(username){
+   const user = await database.query(`
     SELECT 
-        * 
+      * 
     FROM 
-        users
-    `);
-    return result.rows[0];
-};
+      users
+    WHERE
+      username = $1;
+
+    `, [username]);
+    
+    return user.rows[0];
+}
 
 module.exports = {
-    testQuery
+    getUserByUsername,
+
 }
