01149ddc044847e71b6ca525407a9df1f5a390c0
annelinets <118258673+annelinets@users.noreply.github.com>
2023-02-09 09:24:32 +0100

    Create functions.js

---
+++ services/functions.js
@@ -0,0 +1,37 @@
+
+export function turnJsonToObjectArray(result) {
+
+    let dates = result.data[0].coordinates[0].dates.map(date => {
+        return date.date
+    }, [])
+
+    let allParams = result.data.map(param => {
+        return param.parameter
+    })
+
+    let allValues = result.data.map(param => {
+        let values = param.coordinates[0].dates.map(param => {
+            return param.value;
+        })
+        return values
+    })
+
+    let everything = dates.map((date, i) => {
+        let paramWithValue = allParams.map((param, indexparam) => {
+            let value = allValues.map((value) => {
+                return value[i]
+            })
+
+            return {
+                [param]: value[indexparam]
+            }
+
+        })
+        return {
+            date: date,
+            parameters: paramWithValue
+
+        }
+    }, [])
+    return everything;
+}
