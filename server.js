066f9645e725a0f94bfaba21b2337b7d586e6187
EirikRH <eirikrhansen@gmail.com>
2023-02-13 14:20:25 +0100

    loker med /session

--- server.js
+++ server.js
@@ -67,11 +67,11 @@ app.post('/login', async (req, res) => {
 });
 
 app.get('/session', async (req, res) => {
-	const token = req.headers['x-token'];
+	const token = req.headers.token;
 
 	try {
 		const payload = jwt.verify(token, Buffer.from(APP_SECRET, 'Base64'));
-		res.json({message: `You are logged in as ${payload.name}`});
+		res.json({message: `You are logged in as ${payload.email}`});
 	} catch (error) {
 		res.status(401).send({error: 'Invalid token'});
 	}
